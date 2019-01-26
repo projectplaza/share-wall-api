e9aa24d2048b6a5700c21d368342c0a0f27c9df3
Ishigami <gumi0077udc@gmail.com>
2019-01-26 19:09:58 +0900

    チェック処理追加

--- routes/login.js
+++ routes/login.js
@@ -6,8 +6,21 @@ var router = express.Router();
  */
 /* POST. */
 router.post('/', function(req, res, next) {
+  // チェック処理
+  let params = req.body;
+  if (params.userId == undefined || params.userId == "") {
+    res.status(500).send({message : "ユーザIDが入力されていません。"});
+  }
+  if (params.password == undefined || params.password == "") {
+    res.status(500).send({message : "パスワードが入力されていません。"});
+  }
+  // if (false) {
+  //   res.status(500).send({error : "IDかパスワードが間違っています。"});
+  // }
+
+  // トークン取得
   console.log(req)
-  res.send({token : "トークン"});
+  res.send({token : "key"});
 });
 
 module.exports = router;
